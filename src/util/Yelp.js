80114f83c21fb3a45a5825f0642b7a1b8420441f
OGReabon <82502110+OGReabon@users.noreply.github.com>
2021-07-13 12:29:36 +0200

    Add files via upload

---
+++ src/util/Yelp.js
@@ -0,0 +1,37 @@
+
+
+const apiKey = '8wIKTNGrvSd0QxKaCtWAFQVCn4laUIFTpKsK_Zz8i4pJfaiHgRo__ZZWN2iAZeFBbcEan4CMZP_H6_xw8oVjBBvk_B7xIcAh8K61XGqt6FtQx_U_eCrqjGrUgIWjYHYx';
+
+const Yelp = {
+    searchYelp(term, location, sortBy){
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+        {headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        },
+        ).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    console.log(business);
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating:business.rating,
+                        reviewCount: business.reviewCount,
+                    };
+                });
+            }
+        });
+    }
+    
+};
+
+export default Yelp;
